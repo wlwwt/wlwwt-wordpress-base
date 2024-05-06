15ed392667b2fb424422304ef7637974a52d6a91
Wolfgang Freund <wolfgang.freund@wlwwt.de>
2024-05-06 19:53:56 +0200

    added theme fixed to project

---
+++ public/wp-content/themes/kadence/assets/js/admin/customizer.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1b416ac7ae29d3d81440');
